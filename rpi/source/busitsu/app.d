9e7d99949a67ffefd86bc67fe4ecace6ec1f7625
YamaD <me@dyama.net>
2021-12-29 17:22:47 +0900

    rpi

---
+++ rpi/source/busitsu/app.d
@@ -0,0 +1,11 @@
+import std;
+import dlog;
+import busitsu.hidapi;
+
+void main() {
+  Logger logger = new DefaultLogger();
+
+  logger.log("booting up...");
+
+  logger.log("exit...");
+}
---
+++ rpi/source/busitsu/serial.d
@@ -0,0 +1,1 @@
+module busitsu.serial;
